1fb552eb618526c4d37104627837cfe40bea52ca
mpcooper23 <mpcooper23@gmail.com>
2024-10-31 15:05:41 -0500

    10:31 3:05

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -9,12 +9,28 @@
  *  b. BUT, for multiples of three print “Fizz” instead of the number.
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
+ * 
+ * I: numbers data
+ * O: printing 1 - 100 with "Fizz" assigned to any multiples of 3, multiples of 5 print "FizzBuzz"
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
+    //initializing var array to contain numbers
+    let nums = [];
+//creating for loop to loop through numbers
+for(let i = 1; i < 100; i++){
+    
+    nums.push(numbers[i])
+    //creating conditional if statement to pull multiples of three
+} if (numbers[i] % 3 === 0){
+    return console.log("Fizz")
+}if (numbers[i] % 5 === 0){
+    return console.log("Buzz")
+}if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0){
+    return console.log("FizzBuzz")
+}
 
-
-
+return nums
     // YOUR CODE GOES ABOVE HERE //
 }
 
